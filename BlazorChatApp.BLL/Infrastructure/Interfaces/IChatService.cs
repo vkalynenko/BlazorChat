f3fafed2d68d6f8987b1e5f73b3e20fa20d65a55
Valeriia <valeria170503@gmail.com>
2022-05-17 17:50:31 +0300

    Architecture

---
+++ BlazorChatApp.BLL/Infrastructure/Interfaces/IChatService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorChatApp.BLL.Infrastructure.Interfaces
+{
+    public interface IChatService
+    {
+
+    }
+}
---
+++ BlazorChatApp.DAL/Domain/Entities/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorChatApp.DAL.Domain.Entities
+{
+    public class Message
+    {
+        private string text;
+
+        public int MessageId { get; set; }
+        public string MessageText { get; set; }
+
+        public DateTime SenTime { get; set; }
+    }
+}
