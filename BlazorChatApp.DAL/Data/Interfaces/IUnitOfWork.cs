8c46216bd4c83d87ed8e9a32720311d93174c704
Valeriia <valeria170503@gmail.com>
2022-05-19 18:59:00 +0300

    Unit of work

--- BlazorChatApp.DAL/Data/Interfaces/IUnitOfWork.cs
+++ BlazorChatApp.DAL/Data/Interfaces/IUnitOfWork.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BlazorChatApp.DAL.Data.Repositories;
 
 namespace BlazorChatApp.DAL.Data.Interfaces
 {
--- BlazorChatApp.DAL/Data/UnitOfWork.cs
+++ BlazorChatApp.DAL/Data/UnitOfWork.cs
@@ -7,25 +7,45 @@ namespace BlazorChatApp.DAL.Data
     public class UnitOfWork : IUnitOfWork
     {
         private readonly BlazorChatAppContext _context;  
-        private GenericRepository<Chat> chatRepository;
-        private GenericRepository<Message> messageRepository;
+        private GenericRepository<Chat> _chatRepository;
+        private GenericRepository<Message> _messageRepository;
 
-        public UnitOfWork(BlazorChatAppContext appContext)
+        public UnitOfWork(
+            BlazorChatAppContext appContext, 
+            GenericRepository<Chat> chatRepository, 
+            GenericRepository<Message> messageRepository)
         {
             _context = appContext;
+            _chatRepository = chatRepository;
+            _messageRepository = messageRepository;
         }
+
         public GenericRepository<Chat> ChatRepository
         {
             get
             {
 
-                if (this.chatRepository == null)
+                if (_chatRepository == null)
                 {
-                    this.chatRepository = new GenericRepository<Chat>(_context);
+                    _chatRepository = new GenericRepository<Chat>(_context);
                 }
-                return chatRepository;
+                return _chatRepository;
             }
         }
+
+        public GenericRepository<Message> MessageRepository
+        {
+            get
+            {
+
+                if (_messageRepository == null)
+                {
+                    _messageRepository = new GenericRepository<Message>(_context);
+                }
+                return _messageRepository;
+            }
+        }
+
 
         public async Task<int> SaveChanges()
         {
